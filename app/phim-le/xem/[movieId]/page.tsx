cb169b823ec4bf5133f25c163c85c6d5be2dd4da
Tung Pham <tung.42@gmail.com>
2025-06-01 17:29:37 +0700

    render

--- app/phim-le/xem/[movieId]/page.tsx
+++ app/phim-le/xem/[movieId]/page.tsx
@@ -34,7 +34,7 @@ interface Movie {
 // Environment variables with type assertion
 const API_KEY = "fecb69b9d0ad64dbe0802939fafc338d"!;
 const BASE_URL = "https://api.themoviedb.org/3"!;
-const EMBED_URL = "http://thegioiphim.getenjoyment.net/player.php"!;
+const EMBED_URL = "https://se-player.onrender.com"!;
 
 // Reusable component for movie information
 const MovieInfo: React.FC<{ movie: Movie }> = ({ movie }) => (
